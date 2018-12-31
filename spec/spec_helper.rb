642d309135b21c54d55148ae90adf52026f3ad6c
Jared Norman <jared@super.gd>
2018-12-30 17:53:23 -0800

    Bring in everyone's favourite RSpec options

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,15 @@ require "bundler/setup"
 require "super_good/solidus_taxjar"
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
-  config.disable_monkey_patching!
-
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.disable_monkey_patching!
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+  config.default_formatter = 'doc' if config.files_to_run.one?
+
+  config.order = "random"
+  Kernel.srand config.seed
 end

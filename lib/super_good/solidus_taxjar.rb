92844e94a4db4a6e6d6b1ec68ac42e535c1116fa
Jared Norman <jared@super.gd>
2018-12-30 17:42:23 -0800

    Start requiring things we need

--- lib/super_good/solidus_taxjar.rb
+++ lib/super_good/solidus_taxjar.rb
@@ -1,6 +1,5 @@
-require "super_good/solidus_taxjar/version"
+require 'solidus_core'
+require 'solidus_support'
+require 'taxjar'
 
-module SuperGood
-  module SolidusTaxJar
-  end
-end
+require "super_good/solidus_taxjar/version"

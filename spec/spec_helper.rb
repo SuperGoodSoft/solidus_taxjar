b1d9f590860a74831d594dec5464b67f3aa6e18f
Jared Norman <jared@super.gd>
2019-01-09 16:38:01 -0800

    Use DatabaseCleaner to keep the database clean

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 
 require "bundler/setup"
+require "database_cleaner"
 
 begin
   require File.expand_path('../dummy/config/environment', __FILE__)
@@ -21,4 +22,17 @@ RSpec.configure do |config|
 
   config.order = "random"
   Kernel.srand config.seed
+
+  config.before :suite do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before :each do
+    DatabaseCleaner.start
+  end
+
+  # After each spec clean the database.
+  config.after :each do
+    DatabaseCleaner.clean
+  end
 end

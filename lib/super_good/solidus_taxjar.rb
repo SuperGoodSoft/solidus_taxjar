462aa5b334c44e7cef9536ebf0db2e98875031da
Jared Norman <jared@super.gd>
2019-01-24 17:58:39 -0800

    Add configuration for discount calculator
    This allows stores to specify their own discount calculator in an
    initializer, by doing something like the following:
    
    SuperGood::SolidusTaxJar.discount_calculator = MyStore::CustomDiscountCalculator
    
    The class's constructor should take in a line item and have a #discount
    method that returns the discount amount (which should be a negative
    number.)

--- lib/super_good/solidus_taxjar.rb
+++ lib/super_good/solidus_taxjar.rb
@@ -2,11 +2,17 @@ require 'solidus_core'
 require 'solidus_support'
 require 'taxjar'
 
-module SuperGood
-  module SolidusTaxJar
-  end
-end
-
 require "super_good/solidus_taxjar/version"
 require "super_good/solidus_taxjar/api"
 require "super_good/solidus_taxjar/tax_calculator"
+require "super_good/solidus_taxjar/discount_calculator"
+
+module SuperGood
+  module SolidusTaxJar
+    class << self
+      attr_accessor :discount_calculator
+    end
+
+    self.discount_calculator = ::SuperGood::SolidusTaxJar::DiscountCalculator
+  end
+end

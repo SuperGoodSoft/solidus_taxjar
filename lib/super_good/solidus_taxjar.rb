d22574e1bb948cf140b43d1112e93b4562337484
Jared Norman <jared@super.gd>
2019-03-24 09:42:37 -0700

    Sort config options
    I just like any list with more than half a dozen things to be sorted.

--- lib/super_good/solidus_taxjar.rb
+++ lib/super_good/solidus_taxjar.rb
@@ -12,20 +12,20 @@ module SuperGood
   module SolidusTaxJar
     class << self
       attr_accessor :discount_calculator
-      attr_accessor :test_mode
       attr_accessor :exception_handler
-      attr_accessor :taxable_address_check
-      attr_accessor :shipping_tax_label_maker
       attr_accessor :line_item_tax_label_maker
+      attr_accessor :shipping_tax_label_maker
+      attr_accessor :taxable_address_check
+      attr_accessor :test_mode
     end
 
     self.discount_calculator = ::SuperGood::SolidusTaxJar::DiscountCalculator
-    self.test_mode = false
     self.exception_handler = ->(e) {
       Rails.logger.error "An error occurred while fetching TaxJar tax rates - #{e}: #{e.message}"
     }
-    self.taxable_address_check = ->(address) { true }
-    self.shipping_tax_label_maker = ->(shipment, shipping_tax) { "Sales Tax" }
     self.line_item_tax_label_maker = ->(taxjar_line_item, spree_line_item) { "Sales Tax" }
+    self.shipping_tax_label_maker = ->(shipment, shipping_tax) { "Sales Tax" }
+    self.taxable_address_check = ->(address) { true }
+    self.test_mode = false
   end
 end

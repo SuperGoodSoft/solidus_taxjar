f810c06de2bc53d0b8f36ec9752aa924829cbf5c
Noah Silvera <noah@super.gd>
2021-08-18 15:16:24 -0700

    Add required address parameters for refund
    We need to post the address  associated with the order for a refund
    transaction. We have assumed the refund should reflect the most up
    to date address, so we pull it from the spree order and not the taxjar
    transaction.
    
    Co-authored-by: Nick Van Doorn <nick@super.gd>

--- lib/super_good/solidus_taxjar/api_params.rb
+++ lib/super_good/solidus_taxjar/api_params.rb
@@ -52,14 +52,18 @@ module SuperGood
         end
 
         def refund_transaction_params(spree_order, taxjar_order)
-          {
-            transaction_id: spree_order.number + "-REFUND-" + SecureRandom.hex(10),
-            transaction_reference_id: spree_order.number,
-            transaction_date: spree_order.completed_at.to_formatted_s(:iso8601),
-            amount: -1 * taxjar_order.amount,
-            sales_tax: -1 * taxjar_order.sales_tax,
-            shipping: -1 * taxjar_order.shipping
-          }
+          {}
+            .merge(order_address_params(spree_order.tax_address))
+            .merge(
+              {
+                transaction_id: spree_order.number + "-REFUND-" + SecureRandom.hex(10),
+                transaction_reference_id: spree_order.number,
+                transaction_date: spree_order.completed_at.to_formatted_s(:iso8601),
+                amount: -1 * taxjar_order.amount,
+                sales_tax: -1 * taxjar_order.sales_tax,
+                shipping: -1 * taxjar_order.shipping
+              }
+            )
         end
 
         def refund_params(reimbursement)

4d53145a35fc29c18c86c694a332476e81851a14
Senem Soy <senem@super.gd>
2021-09-08 16:14:41 -0700

    Add api for creating transaction from order
    The update logic for updating the transaction if it already exists is
    not there yet, will be implemented later.
    
    Co-authored-by: Noah Silvera <noah@super.gd>
    Co-authored-by: Adam Mueller <adam@super.gd>

--- lib/super_good/solidus_taxjar.rb
+++ lib/super_good/solidus_taxjar.rb
@@ -11,6 +11,7 @@ require "super_good/solidus_taxjar/tax_calculator"
 require "super_good/solidus_taxjar/tax_rate_calculator"
 require "super_good/solidus_taxjar/discount_calculator"
 require "super_good/solidus_taxjar/addresses"
+require "super_good/solidus_taxjar/reporting"
 
 module SuperGood
   module SolidusTaxjar

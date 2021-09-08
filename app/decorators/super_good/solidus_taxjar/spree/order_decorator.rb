ebf147b299a481d14d0104d9510e7e53296ab794
benjamin wil <benjamin@super.gd>
2021-09-08 14:31:35 -0700

    Associate `Spree::Order`s with taxjar order transactions
    A Spree::Order can be associated with multiple taxjar order
    transactions, each representing a taxable event for the order.
    
    Co-authored-by: Nick Van Doorn <nick@super.gd>
    Co-authored-by: Benjamin Willems <benjamin@super.gd>
    Co-authored-by: Chris Todorov <chris@super.gd>

---
+++ app/decorators/super_good/solidus_taxjar/spree/order_decorator.rb
@@ -0,0 +1,16 @@
+module SuperGood
+  module SolidusTaxjar
+    module Spree
+      module OrderDecorator
+        def self.prepended(base)
+          base.has_many :taxjar_order_transactions,
+            class_name: "SuperGood::SolidusTaxjar::OrderTransaction",
+            dependent: :destroy,
+            inverse_of: :order
+        end
+
+        ::Spree::Order.prepend self
+      end
+    end
+  end
+end

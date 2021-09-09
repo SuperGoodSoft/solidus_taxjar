b89cabe52ca63b765aa60d182291c6095e695c10
Noah Silvera <noah@super.gd>
2021-09-09 14:15:25 -0700

    Run migrations in install generator
    Now that this extension has introduced a custom table, we need to run
    the migrations on install.
    
    Co-authored-by: Nick Van Doorn <nick@super.gd>
    Co-authored-by: Benjamin Willems <benjamin@super.gd>
    Co-authored-by: Chris Todorov <chris@super.gd>

--- lib/generators/super_good/solidus_taxjar/install_generator.rb
+++ lib/generators/super_good/solidus_taxjar/install_generator.rb
@@ -2,6 +2,8 @@ module SuperGood
   module SolidusTaxjar
     module Generators
       class InstallGenerator < Rails::Generators::Base
+        class_option :auto_run_migrations, type: :boolean, default: false
+
         def create_initializer_file
           solidus_initializer_path = "config/initializers/solidus.rb"
 
@@ -11,6 +13,19 @@ module SuperGood
               config.tax_calculator_class = SuperGood::SolidusTaxjar::TaxCalculator
             end
           INIT
+        end
+
+        def add_migrations
+          run 'bin/rails railties:install:migrations FROM=solidus_taxjar'
+        end
+
+        def run_migrations
+          run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask('Would you like to run the migrations now? [Y/n]'))
+          if run_migrations
+            run 'bin/rails db:migrate'
+          else
+            puts 'Skipping bin/rails db:migrate, don\'t forget to run it!'
+          end
         end
       end
     end

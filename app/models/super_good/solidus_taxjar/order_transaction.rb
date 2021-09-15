2e267728da484bde5ae61ddbe8bd5ee27cfaf518
benjamin wil <benjamin@super.gd>
2021-09-15 14:08:18 -0700

    Add `OrderTransaction.latest_for(order)` method
    This method lets us get the latest TaxJar order transaction for the
    order in a more ergonomic and fool-proof way.
    
    Co-Authored-By: Chris Todorov <chris@super.gd>

--- app/models/super_good/solidus_taxjar/order_transaction.rb
+++ app/models/super_good/solidus_taxjar/order_transaction.rb
@@ -5,6 +5,10 @@ module SuperGood
 
       validates_presence_of :transaction_id
       validates_presence_of :transaction_date
+
+      def self.latest_for(order)
+        where(order: order).order(transaction_date: :desc).limit(1).first
+      end
     end
   end
 end
---
+++ spec/models/super_good/solidus_taxjar/order_transaction_spec.rb
@@ -0,0 +1,34 @@
+require "spec_helper"
+
+RSpec.describe SuperGood::SolidusTaxjar::OrderTransaction do
+  describe ".latest_for" do
+    subject { described_class.latest_for(order) }
+
+    let(:order) { create(:order) }
+
+    context "when there are no order transactions" do
+      it { is_expected.to be_nil }
+    end
+
+    context "when there are one or more order transactions" do
+      let!(:first_order_transaction) {
+        create(
+          :taxjar_order_transaction,
+          transaction_date: 2.days.ago,
+          order: order
+        )
+      }
+      let!(:latest_order_transaction) {
+        create(
+          :taxjar_order_transaction,
+          transaction_date: 1.days.ago,
+          order: order
+        )
+      }
+
+      it "returns the most recent order transaction" do
+        expect(subject).to eq latest_order_transaction
+      end
+    end
+  end
+end

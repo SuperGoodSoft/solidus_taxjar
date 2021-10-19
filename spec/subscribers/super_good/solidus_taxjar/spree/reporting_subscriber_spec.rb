e85d5f4dd36d1941ea07d9dc80314fe15ba51215
Senem Soy <senem@super.gd>
2021-10-19 11:28:44 -0700

    Only report transaction if reporting is enabled
    Being able to enable and disable global reporting is one of the
    certification requirements for TaxJar.
    
    Co-authored-by: Noah Silvera <noah@super.gd>
    Co-authored-by: Chris Todorov <chris@super.gd>

--- spec/subscribers/super_good/solidus_taxjar/spree/reporting_subscriber_spec.rb
+++ spec/subscribers/super_good/solidus_taxjar/spree/reporting_subscriber_spec.rb
@@ -4,21 +4,47 @@ RSpec.describe SuperGood::SolidusTaxjar::Spree::ReportingSubscriber do
   describe "shipment_shipped is fired" do
     subject { ::Spree::Event.fire 'shipment_shipped', shipment: shipment  }
 
-    let(:shipment) { create(:shipment, state: 'ready', order: order) }
-    let(:order) { create :order_with_line_items }
+    let(:shipment) { build_stubbed(:shipment, state: 'ready', order: order) }
+    let(:order) { build_stubbed :order_with_line_items }
+    let(:reporting) { instance_spy(::SuperGood::SolidusTaxjar::Reporting) }
 
-    it "reports transaction" do
-      reporting = instance_spy(::SuperGood::SolidusTaxjar::Reporting)
+    context "reporting is enabled" do
+      before do
+        allow(::SuperGood::SolidusTaxjar)
+          .to receive(:reporting_enabled)
+          .and_return(true)
 
-      allow(::SuperGood::SolidusTaxjar)
-        .to receive(:reporting)
-        .and_return(reporting)
+        allow(::SuperGood::SolidusTaxjar)
+          .to receive(:reporting)
+          .and_return(reporting)
+      end
 
-      expect(reporting)
-        .to receive(:report_transaction)
-        .with(shipment.order)
+      it "reports transaction" do
+        expect(reporting)
+          .to receive(:report_transaction)
+          .with(shipment.order)
 
-      subject
+        subject
+      end
+    end
+
+    context "reporting is disabled" do
+      before do
+        allow(::SuperGood::SolidusTaxjar)
+          .to receive(:reporting_enabled)
+          .and_return(false)
+      end
+
+      it "returns nothing" do
+        expect(subject).to be_nil
+      end
+
+      it "doesn't report the transaction" do
+        expect(reporting)
+          .to_not receive(:report_transaction)
+
+        subject
+      end
     end
   end
 end

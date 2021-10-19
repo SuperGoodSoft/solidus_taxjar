508d21109a105c9d734a2e6494362e07dbcb4be1
Senem Soy <senem@super.gd>
2021-10-19 11:28:44 -0700

    Only report transaction if reporting is enabled
    Being able to enable and disable global reporting is one of the
    certification requirements for TaxJar.
    
    Co-authored-by: Noah Silvera <noah@super.gd>
    Co-authored-by: Chris Todorov <chris@super.gd>

--- app/subscribers/super_good/solidus_taxjar/spree/reporting_subscriber.rb
+++ app/subscribers/super_good/solidus_taxjar/spree/reporting_subscriber.rb
@@ -6,6 +6,7 @@ module SuperGood
         event_action :report_transaction, event_name: :shipment_shipped
 
         def report_transaction(event)
+          return unless SuperGood::SolidusTaxjar.reporting_enabled
           SuperGood::SolidusTaxjar.reporting.report_transaction(event.payload[:shipment].order)
         end
       end

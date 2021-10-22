2451a5d59f2f9ddd459ec076a9aeff0f374f14c8
Senem Soy <senem@super.gd>
2021-10-22 09:58:23 -0700

    Move the install generator to the correct folder
    The install generator wasn't working for the dummy app because it wasn't
    in the correctfolder. The change in the Rakefile fixes the namespace, and
    these both changes together make the install generator actually run when
    the dummy app is generated.
    
    One side effect of this change is that now the dummy app uses the TaxJar
    tax calculator, which instantiates the api client. That will fail if
    there is no TAXJAR_API_KEY environment configured. For this reason we
    need to stub the creation of the api client in tests that calculate
    taxes.
    
    Co-authored-by: Chris Todorov <chris@super.gd>
    Co-authored-by: Adam Mueller <adam@super.gd>

--- spec/models/spree/shipment_spec.rb
+++ spec/models/spree/shipment_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe Spree::Shipment do
     let(:shipment) { create(:shipment, state: 'ready', order: order) }
     let(:order) { create :order_with_line_items }
 
+    before do
+      # Prevent client from being instantiated in test environment.
+      allow(SuperGood::SolidusTaxjar).to receive(:api)
+    end
+
     it 'fires the shipment_shipped event exactly once' do
       stub_const('Spree::Event', class_spy(Spree::Event))
 

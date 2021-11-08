2490559d16dd0eee5178548f75f09fc065645831
Noah Silvera <noah@super.gd>
2021-11-08 13:33:31 -0800

    tmp

--- spec/models/spree/order_updater_spec.rb
+++ spec/models/spree/order_updater_spec.rb
@@ -1,10 +1,5 @@
 RSpec.describe Spree::OrderUpdater do
   describe '#update' do
-    before do
-      # Prevent client from being instantiated in test environment.
-      allow(SuperGood::SolidusTaxjar).to receive(:api)
-    end
-
     it 'fires the order_recalculated event exactly once' do
       stub_const('Spree::Event', class_spy(Spree::Event))
       order = create(:order)

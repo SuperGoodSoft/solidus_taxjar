b4a78df21b48ed448e91632726781eedc3ecf2bb
Noah Silvera <noah@super.gd>
2021-11-22 13:57:13 -0800

    Configure VCR for feature tests
    A feature test that calls out to the real TaxJar api will be added
    in a future commit, so we should configure VCR to allow us to record
    that request.
    
    Co-authored-by: Nick Van Doorn <nick@super.gd>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,7 @@ require File.expand_path("dummy/config/environment.rb", __dir__).tap { |file|
 }
 
 require "solidus_dev_support/rspec/feature_helper"
+require 'vcr'
 
 factories = Dir["#{::SuperGoodSolidusTaxjar::Engine.root}/lib/super_good/solidus_taxjar/testing_support/factories/**/*_factory.rb"].sort
 factories.each { |f| require f }
@@ -20,4 +21,21 @@ Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each { |f| require f }
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.use_transactional_fixtures = false
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/cassettes'
+  config.ignore_localhost = true
+  config.configure_rspec_metadata!
+  config.hook_into :webmock
+  driver_urls = Webdrivers::Common.subclasses.map do |driver|
+    Addressable::URI.parse(driver.base_url).host
+  end
+  config.ignore_hosts("chromedriver.storage.googleapis.com", *driver_urls)
+  config.filter_sensitive_data('<BEARER_TOKEN>') { |interaction|
+    auths = interaction.request.headers['Authorization'].first
+    if (match = auths.match /^Bearer\s+([^,\s]+)/ )
+      match.captures.first
+    end
+  }
 end

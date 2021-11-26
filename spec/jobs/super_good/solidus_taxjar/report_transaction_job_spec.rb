47c4a6fbac7b557020260300faab2329ca20ad13
Senem Soy <senem@super.gd>
2021-11-26 11:35:59 -0800

    Create a job to report transactions
    This will be used to ensure we report transactions async and can be used
    in the future to backfill order transactions.
    
    Co-authored-by: Chris Todorov <chris@super.gd>

---
+++ spec/jobs/super_good/solidus_taxjar/report_transaction_job_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper"
+
+RSpec.describe SuperGood::SolidusTaxjar::ReportTransactionJob do
+  describe ".perform_later" do
+    subject { described_class.perform_later(order) }
+
+    let(:order) { create :order }
+    let(:mock_reporting) { instance_double ::SuperGood::SolidusTaxjar::Reporting }
+
+    it "enqueues the job" do
+      assert_enqueued_with(job: described_class, args: [order]) do
+        subject
+      end
+    end
+
+    it "reports the transaction when it performs the job" do
+      allow(SuperGood::SolidusTaxjar).to receive(:reporting).and_return(mock_reporting)
+      expect(mock_reporting).to receive(:report_transaction).with(order)
+
+      perform_enqueued_jobs do
+        subject
+      end
+    end
+  end
+end

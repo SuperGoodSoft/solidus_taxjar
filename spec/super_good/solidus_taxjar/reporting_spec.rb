101558068fb9611a3300e9807713e9d4413af160
benjamin wil <benjamin@super.gd>
2021-12-14 15:19:24 -0800

    Add `Reporting#refund_and_create_new_transaction`
    If a TaxJar transaction already exists for a Solidus order and the order
    totals have changed, we need to cancel (or refund) the existing TaxJar
    transaction and create a new one with the latest Solidus order totals on
    it.
    
    If the refund fails for some reason, we *do not* want to create the new
    transaction. So we're testing that a new transaction is not created in
    that case.
    
    We can use this service method to do that in a child commit.
    
    Co-Authored-By: Alex Blackie <alex@super.gd>
    Co-Authored-By: Mike Conlin <mike@super.gd>

--- spec/super_good/solidus_taxjar/reporting_spec.rb
+++ spec/super_good/solidus_taxjar/reporting_spec.rb
@@ -1,13 +1,60 @@
 require "spec_helper"
 
 RSpec.describe SuperGood::SolidusTaxjar::Reporting do
+  let(:dummy_api) { instance_double ::SuperGood::SolidusTaxjar::Api }
+  let(:order) { build :order, completed_at: 1.days.ago }
+  let(:reporting) { described_class.new(api: dummy_api) }
+
+  describe "#refund_and_create_transaction" do
+    subject { reporting.refund_and_create_new_transaction(order) }
+
+    it "refunds the transaction and creates a new one in TaxJar" do
+      expect(dummy_api)
+        .to receive(:create_refund_transaction_for)
+        .with(order)
+      expect(dummy_api)
+        .to receive(:create_transaction_for)
+        .with(order)
+
+      subject
+    end
+
+    context "when Taxjar cannot create a refund transaction", :vcr do
+      let(:reporting) { described_class.new }
+      let(:order) { create(:completed_order_with_totals) }
+      let!(:tax_rate) { create(:tax_rate, name: "Sales Tax") }
+
+      # We ensure that TaxJar cannot create a refund transaction refunding it
+      # *before* the test scenario.
+      before do
+        SuperGood::SolidusTaxjar.api.create_transaction_for(order)
+        SuperGood::SolidusTaxjar.api.create_refund_transaction_for(order)
+      end
+
+      it "raises an error" do
+        expect { subject }.to raise_error(
+          Taxjar::Error::UnprocessableEntity,
+          "Provider tranx already imported for your user account"
+        )
+      end
+
+      it "doesn't create a new transaction" do
+        expect(SuperGood::SolidusTaxjar.api)
+          .not_to receive(:create_transaction_for)
+
+        begin
+          subject
+        rescue Taxjar::Error::UnprocessableEntity
+          nil
+        end
+      end
+    end
+  end
+
   describe "#report_transaction" do
-    subject { described_class.new(api: dummy_api).report_transaction(order) }
+    subject { reporting.report_transaction(order) }
 
-    let(:dummy_api) { instance_double ::SuperGood::SolidusTaxjar::Api }
-    let(:order) { build :order, completed_at: 1.days.ago }
-
-    it "updates the transaction" do
+    it "shows the latest transaction for the order" do
       allow(dummy_api)
         .to receive(:show_latest_transaction_for)
         .with(order)

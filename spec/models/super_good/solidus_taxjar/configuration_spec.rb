efd418a914265b7d4fb375457316217d63dc496a
Nick Van Doorn <nick@super.gd>
2022-01-21 13:28:16 -0800

    Add default configuration to root module
    Now that we have a model to store dynamic configuration, we want to
    ensure it's used properly in the rest of the gem. We offer a
    convenience method so it's easy to retrieve the right config anywhere in
    the gem.
    
    Co-authored-by: Adam Mueller <adam@super.gd>
    Co-authored-by: Chris Todorov <chris@super.gd>
    Co-authored-by: Alex Blackie <alex@super.gd>
    Co-authored-by: Benjamin Willems <benjamin@super.gd>

--- spec/models/super_good/solidus_taxjar/configuration_spec.rb
+++ spec/models/super_good/solidus_taxjar/configuration_spec.rb
@@ -30,4 +30,33 @@ RSpec.describe SuperGood::SolidusTaxjar::Configuration do
       expect { subject }.to change { taxjar_configuration.reload.preferred_reporting_enabled }.from(false).to(true)
     end
   end
+
+  describe ".default" do
+    subject { described_class.default }
+
+    context "configuration exists" do
+      let!(:taxjar_configuration) { create :taxjar_configuration }
+
+      it "returns the default configuration" do
+        expect(subject).to eq(taxjar_configuration)
+      end
+    end
+
+    context "configuration doesn't exist" do
+      it "creates the default config" do
+        expect { subject }.to change { described_class.count }.from(0).to(1)
+      end
+
+      it "returns a config" do
+        expect(subject).to be_instance_of(described_class)
+      end
+
+      context "calling default more than once" do
+        it "creates only one config" do
+          described_class.default
+          expect { subject }.to_not change { described_class.count }.from(1)
+        end
+      end
+    end
+  end
 end

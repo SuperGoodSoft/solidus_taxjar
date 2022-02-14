46844575f764a70d9125c30ae5e1fa2f4e532dd7
Noah Silvera <noah@super.gd>
2022-02-14 14:38:05 -0800

    Add implementation for backfilling transactions
    In this naive implementation, any orders which have not previously been
    reported and are complete should be reported to taxjar. We currently
    don't worry about whether a reported order is up to date.

--- lib/super_good/solidus_taxjar/backfill_transactions.rb
+++ lib/super_good/solidus_taxjar/backfill_transactions.rb
@@ -1,8 +1,23 @@
 module SuperGood
   module SolidusTaxjar
     class BackfillTransactions
-      def self.call
+      def initialize(api: SuperGood::SolidusTaxjar.api)
+        @api = api
       end
+
+      def call
+        backfilled_orders = []
+        ::Spree::Order.complete.where(shipment_state: 'shipped').find_each do |order|
+          next if order.taxjar_order_transactions.any?
+          api.create_transaction_for(order)
+          backfilled_orders.push(order.number)
+        end
+        backfilled_orders
+      end
+
+      private
+
+      attr_reader :api
     end
   end
 end

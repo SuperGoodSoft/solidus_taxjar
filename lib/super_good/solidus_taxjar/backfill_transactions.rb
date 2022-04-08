cb5f70248bafbab430afdcbc50df34de6d382207
Noah Silvera <noah@super.gd>
2022-04-08 15:19:49 -0700

    Create database logs for each backfilled transaction
    When a order is synced to TaxJar through a transaction backfill, we want
    to create an associated log in the database to allow the results to be
    viewed at anytime.
    
    Co-authored-by: Adam Mueller <adam@super.gd>

--- lib/super_good/solidus_taxjar/backfill_transactions.rb
+++ lib/super_good/solidus_taxjar/backfill_transactions.rb
@@ -7,9 +7,20 @@ module SuperGood
 
       def call
         backfilled_orders = []
+        transaction_sync_batch = SuperGood::SolidusTaxjar::TransactionSyncBatch.create!
+
         ::Spree::Order.complete.where(shipment_state: 'shipped').find_each do |order|
           next if order.taxjar_order_transactions.any?
-          api.create_transaction_for(order)
+          transaction_sync_log = SuperGood::SolidusTaxjar::TransactionSyncLog.create!(
+            transaction_sync_batch: transaction_sync_batch,
+            order: order
+          )
+          begin
+            order_transaction = SuperGood::SolidusTaxjar.reporting.report_transaction(order)
+            transaction_sync_log.update!(order_transaction: order_transaction, status: :success)
+          rescue Taxjar::Error => exception
+            transaction_sync_log.update!(status: :error, error_message: exception.message)
+          end
           backfilled_orders.push(order.number)
         end
         backfilled_orders
--- spec/super_good/solidus_taxjar/backfill_transactions_spec.rb
+++ spec/super_good/solidus_taxjar/backfill_transactions_spec.rb
@@ -5,7 +5,9 @@ RSpec.describe SuperGood::SolidusTaxjar::BackfillTransactions do
     subject { described_class.new(api: api_spy).call }
 
     let!(:shipped_order) { create :shipped_order }
+    let(:reporting_mock) { instance_double ::SuperGood::SolidusTaxjar::Reporting }
     let(:api_spy) { instance_spy(::SuperGood::SolidusTaxjar::Api) }
+    let(:test_transaction_id) { "R1234-transaction" }
 
     around do |example|
       ::SuperGood::SolidusTaxjar.test_mode = true
@@ -17,16 +19,58 @@ RSpec.describe SuperGood::SolidusTaxjar::BackfillTransactions do
       reported_order = create :shipped_order
       create(:taxjar_order_transaction, order: reported_order)
       create :order_ready_to_ship
-    end
 
-    it "reports all completed and shipped orders to taxjar" do
-      subject
-      expect(api_spy).to have_received(:create_transaction_for).once
-      expect(api_spy).to have_received(:create_transaction_for).with(shipped_order)
+      unreported_transaction = build(:taxjar_order_transaction, order: shipped_order, transaction_id: test_transaction_id)
+
+      allow(SuperGood::SolidusTaxjar).to receive(:reporting).and_return(reporting_mock)
+      allow(reporting_mock).to receive(:report_transaction).with(shipped_order) do
+        unreported_transaction.save!
+        unreported_transaction
+      end
     end
 
     it "returns the numbers of the orders that have been pushed" do
       expect(subject).to eq([shipped_order.number])
+    end
+
+    it "creates a log of each synced order in the database" do
+      expect { subject }.to change { SuperGood::SolidusTaxjar::TransactionSyncBatch.count }
+        .from(0).to(1)
+      transaction_sync_batch = SuperGood::SolidusTaxjar::TransactionSyncBatch.last
+      expect(transaction_sync_batch.transaction_sync_logs.count).to eq(1)
+      expect(transaction_sync_batch.transaction_sync_logs.last.order).to eq(shipped_order)
+    end
+
+    it "records the associated order, taxjar transaction, and status on each log" do
+      subject
+      sync_log = SuperGood::SolidusTaxjar::TransactionSyncLog.last
+
+      expect(sync_log).to have_attributes(
+        order: shipped_order,
+        status: "success"
+      )
+      expect(sync_log.order_transaction).not_to be_nil
+    end
+
+    context "when the transaction cannot be created on TaxJar" do
+      before do
+        allow(reporting_mock)
+          .to receive(:report_transaction)
+          .and_raise(Taxjar::Error.new("api down"))
+      end
+
+      it "records a failure status in the log" do
+        subject
+        sync_log = SuperGood::SolidusTaxjar::TransactionSyncLog.last
+
+        expect(sync_log).to have_attributes(
+          order: shipped_order,
+          status: "error",
+          error_message: /api down/
+        )
+
+        expect(sync_log.order_transaction).to be_nil
+      end
     end
   end
 end

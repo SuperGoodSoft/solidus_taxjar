19be7a9b73e95a88d21096798f3ef96705efa7b0
Nick Van Doorn <nick@super.gd>
2022-05-10 14:32:21 -0700

    Add status method to transaction sync batch
    Each transaction sync batch can determine it's status by inspecting the
    associated transaction sync logs. We implement a method to return this
    status for use in a view.
    
    Co-authored-by: Chris Todorov <chris@super.gd>

--- app/models/super_good/solidus_taxjar/transaction_sync_batch.rb
+++ app/models/super_good/solidus_taxjar/transaction_sync_batch.rb
@@ -1,3 +1,10 @@
 class SuperGood::SolidusTaxjar::TransactionSyncBatch < ApplicationRecord
   has_many :transaction_sync_logs
+
+  def status
+    return 'processing' if transaction_sync_logs.processing.any?
+    return 'error' if transaction_sync_logs.error.any?
+
+    'success'
+  end
 end

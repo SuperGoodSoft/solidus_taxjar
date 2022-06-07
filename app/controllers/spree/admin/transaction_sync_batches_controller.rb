ffaa77f7131451f2d79eb2401315c2cd27be11db
Nick Van Doorn <nick@super.gd>
2022-06-07 14:38:41 -0700

    Paginate transaction sync batches
    There will be many transaction sync batches, so we should paginate the
    index page to avoid rendering too many batches.
    
    Co-authored-by: Chris Todorov <chris@super.gd>

--- app/controllers/spree/admin/transaction_sync_batches_controller.rb
+++ app/controllers/spree/admin/transaction_sync_batches_controller.rb
@@ -2,7 +2,7 @@ module Spree
   module Admin
     class TransactionSyncBatchesController < Spree::Admin::BaseController
       def index
-        @batches = SuperGood::SolidusTaxjar::TransactionSyncBatch.all
+        @batches = SuperGood::SolidusTaxjar::TransactionSyncBatch.all.page(params[:page]).per(params[:per_page])
       end
     end
   end
--- spec/features/spree/admin/backfill_transactions_spec.rb
+++ spec/features/spree/admin/backfill_transactions_spec.rb
@@ -7,14 +7,34 @@ RSpec.feature 'Admin Transaction Sync Batches', js: true do
     create :store, default: true
   end
 
-  let!(:transaction_sync_batch) { create :transaction_sync_batch, :with_logs }
+  let!(:transaction_sync_batches) { create_list :transaction_sync_batch, 2, :with_logs }
 
   scenario "renders transaction backfill index" do
-    visit spree.admin_transaction_sync_batches_path
+    visit spree.admin_transaction_sync_batches_path(per_page: 1)
+    transaction_sync_batch = transaction_sync_batches.first
     within "#transaction_sync_batches" do
       expect(page).to have_content(transaction_sync_batch.id)
       expect(page).to have_content(transaction_sync_batch.created_at)
       expect(page).to have_content(transaction_sync_batch.updated_at)
+
+      within "tbody td:nth-child(4)" do
+        expect(page).to have_content("0/1")
+      end
+
+      within "tbody td:nth-child(5)" do
+        expect(page).to have_content("Processing")
+      end
+    end
+
+    within ".pagination" do
+      click_on "Next"
+    end
+
+    second_transaction_sync_batch = transaction_sync_batches.second
+    within "#transaction_sync_batches" do
+      expect(page).to have_content(second_transaction_sync_batch.id)
+      expect(page).to have_content(second_transaction_sync_batch.created_at)
+      expect(page).to have_content(second_transaction_sync_batch.updated_at)
 
       within "tbody td:nth-child(4)" do
         expect(page).to have_content("0/1")

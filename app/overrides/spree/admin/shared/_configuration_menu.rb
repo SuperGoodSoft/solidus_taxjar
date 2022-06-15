e699cb992d31ad5bafd48e41a5cccfb2ea3e17b0
Nick Van Doorn <nick@super.gd>
2022-06-15 14:59:03 -0700

    Add tab for TaxJar backfill
    We should offer a link to this page.
    
    Co-authored-by: Noah Silvera <noah@super.gd>

--- app/overrides/spree/admin/shared/_configuration_menu.rb
+++ app/overrides/spree/admin/shared/_configuration_menu.rb
@@ -7,5 +7,9 @@ Deface::Override.new(
 ) do
   <<-HTML
     <%= configurations_sidebar_menu_item "TaxJar Settings", edit_admin_taxjar_settings_path %>
+
+    <% if SuperGood::SolidusTaxjar.reporting_ui_enabled %>
+      <%= configurations_sidebar_menu_item "TaxJar Backfill", admin_transaction_sync_batches_path %>
+    <% end %>
   HTML
 end

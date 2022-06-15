0902d62ecc7246ed255e685c7326165a8b15763d
Nick Van Doorn <nick@super.gd>
2022-06-15 14:14:07 -0700

    Add transaction sync batch show page
    Now that we're syncing transactions in batches, we should allow users to
    inspect an individual batch and see the status of specific orders.
    
    Co-authored-by: Chris Todorov <chris@super.gd>
    Co-authored-by: Noah Silvera <noah@super.gd>

--- lib/super_good/solidus_taxjar/testing_support/factories/transaction_sync_log_factory.rb
+++ lib/super_good/solidus_taxjar/testing_support/factories/transaction_sync_log_factory.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :transaction_sync_log, class: "SuperGood::SolidusTaxjar::TransactionSyncLog" do
     order
+
+    trait :success do
+      status { "success" }
+
+      after :build do |log|
+        log.order_transaction = build(:taxjar_order_transaction, order: log.order)
+      end
+    end
   end
 end

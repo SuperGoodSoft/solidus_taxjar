bc3150a0cdc0d7d301250cccb47c9e0bad815540
Nick Van Doorn <nick@super.gd>
2022-06-16 14:15:13 -0700

    Move backfill into transaction sync controller
    We have a separate controller for transaction syncing, so we should move
    the backfill action to the controller responsible for managing
    backfilling.
    
    Co-authored-by: Chris Todorov <chris@super.gd>

--- app/controllers/spree/admin/transaction_sync_batches_controller.rb
+++ app/controllers/spree/admin/transaction_sync_batches_controller.rb
@@ -8,6 +8,11 @@ module Spree
       def show
         @batch = SuperGood::SolidusTaxjar::TransactionSyncBatch.find(params[:id])
       end
+
+      def create
+        batch = ::SuperGood::SolidusTaxjar::BackfillTransactions.new.call
+        redirect_to spree.admin_transaction_sync_batch_path(batch)
+      end
     end
   end
 end

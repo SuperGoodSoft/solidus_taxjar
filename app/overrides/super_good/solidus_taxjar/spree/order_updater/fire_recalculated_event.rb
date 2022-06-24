cd258d7e34b2d98ec5d73da003e8479cad866993
Chris Todorov <chris@super.gd>
2022-06-24 13:38:58 -0700

    Update specs for backported event
    The specs here were specific to the legacy implementation of the event
    system, and therefore no longer pass on the `master` branch of Solidus
    where we now use a [new implementation of the event bus](https://github.com/solidusio/solidus/pull/4342).
    This change attempts to refactor the tests to focus on observing the
    side-effects of the object under test which should be independent of the
    underlying implementation.
    
    This test is valuable regardless of what version of Solidus we are running
    against as our extension relies on the event being published when a specific
    action happens and that the payload contains the necessary object.
    
    Co-authored-by: Andrew Stewart <andrew@super.gd>

--- app/overrides/super_good/solidus_taxjar/spree/order_updater/fire_recalculated_event.rb
+++ app/overrides/super_good/solidus_taxjar/spree/order_updater/fire_recalculated_event.rb
@@ -4,6 +4,10 @@ module SuperGood
   module SolidusTaxjar
     module Spree
       module OrderUpdater
+        # Responsible for introducing the `order_recalculated` event for older
+        # versions of Solidus (from 2.9 up until but not including 2.11) which
+        # do not have this event. TaxJar transaction reporting relies on this
+        # event firing when an order is recalculated.
         module FireRecalculatedEvent
           def persist_totals
             ::Spree::Event.fire 'order_recalculated', order: order

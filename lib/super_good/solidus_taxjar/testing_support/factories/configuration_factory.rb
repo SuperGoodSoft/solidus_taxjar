b4193ba6192a1ceda0583a45b3519240905c279e
Noah Silvera <noah@super.gd>
2022-09-12 11:20:28 -0700

    Add basic feature spec for reporting orders to TaxJar
    Reporting order transactions to TaxJar is a key feature and should have
    an associated feature spec.
    
    Co-authored-by: Chris Todorov <chris@super.gd>

--- lib/super_good/solidus_taxjar/testing_support/factories/configuration_factory.rb
+++ lib/super_good/solidus_taxjar/testing_support/factories/configuration_factory.rb
@@ -1,3 +1,7 @@
 FactoryBot.define do
-  factory :taxjar_configuration, class: "SuperGood::SolidusTaxjar::Configuration"
+  factory :taxjar_configuration, class: "SuperGood::SolidusTaxjar::Configuration" do
+    trait :reporting_enabled do
+      preferred_reporting_enabled { true }
+    end
+  end
 end

7828749482a7433d458f528900baf783d202f148
Nick Van Doorn <nick@super.gd>
2022-09-12 21:04:57 -0700

    Associate sync batches with orders
    We frequently write method chains to access this orders through
    transaction sync logs, so we make this easier by adding a has many
    through association.

--- app/models/super_good/solidus_taxjar/transaction_sync_batch.rb
+++ app/models/super_good/solidus_taxjar/transaction_sync_batch.rb
@@ -1,5 +1,6 @@
 class SuperGood::SolidusTaxjar::TransactionSyncBatch < ApplicationRecord
   has_many :transaction_sync_logs
+  has_many :orders, through: :transaction_sync_logs
 
   def status
     return 'processing' if transaction_sync_logs.processing.any?

859938c5f3816cb0cd4eeeacceccbcf39219450f
Noah Silvera <noah@super.gd>
2022-09-23 16:41:02 -0700

    Record transaction sync log for all transactions
    Currently, we are only creating transaction sync logs when we are
    backfilling transactions. We should create them when normal transactions
    are being reported also, as this will allow us to display the logs of
    errors to a user.

--- spec/jobs/super_good/solidus_taxjar/report_transaction_job_spec.rb
+++ spec/jobs/super_good/solidus_taxjar/report_transaction_job_spec.rb
@@ -1,24 +1,57 @@
 require "spec_helper"
 
 RSpec.describe SuperGood::SolidusTaxjar::ReportTransactionJob do
-  describe ".perform_later" do
-    subject { described_class.perform_later(order) }
+  describe ".perform_now" do
+    subject { described_class.perform_now(order) }
 
     let(:order) { create :order }
     let(:mock_reporting) { instance_double ::SuperGood::SolidusTaxjar::Reporting }
+    let(:fake_order_transaction) { create :taxjar_order_transaction }
 
-    it "enqueues the job" do
-      assert_enqueued_with(job: described_class, args: [order]) do
-        subject
-      end
+    before do
+      allow(mock_reporting)
+        .to receive(:show_or_create_transaction)
+        .and_return(fake_order_transaction)
+      allow(SuperGood::SolidusTaxjar).to receive(:reporting).and_return(mock_reporting)
     end
 
     it "reports the transaction when it performs the job" do
-      allow(SuperGood::SolidusTaxjar).to receive(:reporting).and_return(mock_reporting)
-      expect(mock_reporting).to receive(:show_or_create_transaction).with(order)
+      subject
 
-      perform_enqueued_jobs do
-        subject
+      expect(mock_reporting).to have_received(:show_or_create_transaction).with(order)
+    end
+
+    it "creates a transaction sync log with the order transaction" do
+      expect { subject }
+        .to change { ::SuperGood::SolidusTaxjar::TransactionSyncLog.count }
+        .from(0)
+        .to(1)
+
+      expect(::SuperGood::SolidusTaxjar::TransactionSyncLog.last)
+        .to have_attributes(
+          status: "success",
+          order_transaction: fake_order_transaction
+        )
+    end
+
+    context "when syncing to taxjar fails" do
+      before do
+        allow(mock_reporting)
+        .to receive(:show_or_create_transaction)
+        .and_raise(Taxjar::Error.new("something bad"))
+      end
+
+      it "creates a transaction sync log with the error message" do
+        expect { subject }
+          .to change { ::SuperGood::SolidusTaxjar::TransactionSyncLog.count }
+          .from(0)
+          .to(1)
+
+        expect(::SuperGood::SolidusTaxjar::TransactionSyncLog.last)
+          .to have_attributes(
+            status: "error",
+            error_message: "something bad"
+          )
       end
     end
   end

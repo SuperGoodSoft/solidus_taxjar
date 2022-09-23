4ce19f2e2e3d3a8079e7b24f8004abf52e128578
Noah Silvera <noah@super.gd>
2022-09-23 16:41:02 -0700

    Record transaction sync log for all transactions
    Currently, we are only creating transaction sync logs when we are
    backfilling transactions. We should create them when normal transactions
    are being reported also, as this will allow us to display the logs of
    errors to a user.

--- app/jobs/super_good/solidus_taxjar/report_transaction_job.rb
+++ app/jobs/super_good/solidus_taxjar/report_transaction_job.rb
@@ -6,7 +6,15 @@ module SuperGood
       queue_as { SuperGood::SolidusTaxjar.job_queue }
 
       def perform(order)
-        SuperGood::SolidusTaxjar.reporting.show_or_create_transaction(order)
+        transaction_sync_log = SuperGood::SolidusTaxjar::TransactionSyncLog.create!(
+          order: order
+        )
+        begin
+          order_transaction = SuperGood::SolidusTaxjar.reporting.show_or_create_transaction(order)
+          transaction_sync_log.update!(order_transaction: order_transaction, status: :success)
+        rescue Taxjar::Error => exception
+          transaction_sync_log.update!(status: :error, error_message: exception.message)
+        end
       end
     end
   end

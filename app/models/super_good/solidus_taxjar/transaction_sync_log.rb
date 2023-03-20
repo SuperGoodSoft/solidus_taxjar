461aad66f10e63c9a66371635548fe0b778b260b
Noah Silvera <noah.a.silvera@gmail.com>
2023-03-20 14:21:51 -0700

    Associate refund transactions with sync logs
    We want to show the user a reference to the TaxJar refund transaction
    that is created when updating an order in TaxJar.

--- app/models/super_good/solidus_taxjar/transaction_sync_log.rb
+++ app/models/super_good/solidus_taxjar/transaction_sync_log.rb
@@ -2,6 +2,7 @@ class SuperGood::SolidusTaxjar::TransactionSyncLog < ApplicationRecord
   belongs_to :transaction_sync_batch, optional: true
   belongs_to :order, class_name: "Spree::Order"
   belongs_to :order_transaction, optional: true
+  belongs_to :refund_transaction, optional: true
 
   enum status: [:processing, :success, :error]
 end

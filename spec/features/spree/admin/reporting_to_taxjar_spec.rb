f0bf55ac3c1e7355722bff575e719aa3c957d54a
Chris Todorov <chris@super.gd>
2023-04-05 15:46:06 -0700

    Add failing feature spec for retrying failed transaction syncs
    Co-authored-by: benjamin wil <benjamin@super.gd>

--- spec/features/spree/admin/reporting_to_taxjar_spec.rb
+++ spec/features/spree/admin/reporting_to_taxjar_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.feature 'Reporting orders to TaxJar', js: true, vcr: true do
+RSpec.feature 'Reporting orders to TaxJar', js: true, vcr: { allow_unused_http_interactions: false } do
   stub_authorization!
 
   background do
@@ -10,7 +10,7 @@ RSpec.feature 'Reporting orders to TaxJar', js: true, vcr: true do
 
   let!(:order) { create :order_ready_to_ship }
 
-  scenario "shipping a complete and paid order", vcr: { allow_unused_http_interactions: false } do
+  scenario "shipping a complete and paid order" do
     visit spree.edit_admin_order_path(order)
     perform_enqueued_jobs do
       wait_for_order_information_to_load
@@ -20,6 +20,33 @@ RSpec.feature 'Reporting orders to TaxJar', js: true, vcr: true do
 
     expect(page).to have_text("Reported to TaxJar at: #{Date.current.strftime("%B %d, %Y")}", normalize_ws: true)
     expect(page).to have_text("TaxJar Sync: Success", normalize_ws: true)
+  end
+
+  context "with an order with invalid zipcode" do
+    let(:invalid_address) do
+      create(:address, zipcode: 99999)
+    end
+
+    before do
+      order.update!(ship_address: invalid_address)
+    end
+
+    scenario "retry of a previously failed transaction sync" do
+      pending "retry feature is implemented"
+
+      visit spree.edit_admin_order_path(order)
+
+      perform_enqueued_jobs do
+        wait_for_order_information_to_load
+        click_on "Ship"
+        expect(page).to have_content("Shipped package from")
+      end
+
+      within("#order_tab_summary") do
+        expect(page).to have_text("TaxJar Sync: Error", normalize_ws: true)
+        expect(page).to have_text("Retry")
+      end
+    end
   end
 
   def wait_for_order_information_to_load

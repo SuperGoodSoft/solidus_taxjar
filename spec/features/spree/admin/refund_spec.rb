d9953a6ea31c4c70e85381347cefbb311dacf74d
Nick Van Doorn <nick@super.gd>
2023-06-06 13:36:46 -0700

    wip2

---
+++ spec/features/spree/admin/refund_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+RSpec.feature 'Refunding an order', js: true do
+  stub_authorization!
+
+  let(:order) { create(:shipped_order, line_items_count: 3) }
+
+  before do
+    country = create(:country, states_required: true)
+    create(:product, name: "RoR Mug")
+    create(:store, default: true)
+    create(:state, country: country, state_code: "CA")
+    create(:shipping_method)
+    create(:stock_location, name: "Montez's Warehouse")
+    create(:check_payment_method)
+    create(:zone)
+    create :taxjar_configuration, :reporting_enabled
+
+    SuperGood::SolidusTaxjar::ReportTransactionJob.perform_now(order)
+  end
+
+  it "adds tax calculated by TaxJar to the order total", js: true, vcr: {cassette_name: "features/spree/admin/refund", allow_playback_repeats: true, allow_unused_http_interactions: false} do
+    visit spree.admin_order_return_authorizations_path(order)
+
+    binding.pry
+    click_on "New RMA"
+    check "return_authorization[return_items_attributes][0][_destroy]"
+    select "Montez's Warehouse", from: "Stock Location"
+    click_on "Create"
+
+    click_on "Customer Return"
+    click_on "New Customer Return"
+    check "customer_return[return_items_attributes][0][returned]"
+    select "Received", from: "customer_return[return_items_attributes][0][reception_status_event]"
+    select "Montez's Warehouse", from: "Stock Location"
+    click_on "Create"
+    binding.pry
+
+    click_on "Create reimbursement"
+  end
+end

6c90c052bd146253f9b0fe2a0764054f6e39a52b
Chris Todorov <chris@super.gd>
2023-07-12 16:18:11 -0700

    Drop support for load reporting subscriber prior to 2.11
    Solidus 2.11 has been deprecated for a very long time, so we should drop
    this before releasing version 1.0 of this extension.
    
    Co-authored-by: benjamin wil <benjamin@super.gd>

--- lib/super_good/engine.rb
+++ lib/super_good/engine.rb
@@ -5,14 +5,6 @@ module SuperGoodSolidusTaxjar
     isolate_namespace Spree
     engine_name 'super_good_solidus_taxjar'
 
-    # Solidus versions prior to 2.11.0 do not support auto-loading of subscribers
-    # so we need to manually register the reporting subscriber with the event
-    # bus.
-    if Spree.solidus_gem_version < Gem::Version.new('2.11.0')
-      require root.join('app/subscribers/super_good/solidus_taxjar/spree/reporting_subscriber')
-      SuperGood::SolidusTaxjar::Spree::ReportingSubscriber.subscribe!
-    end
-
     include SolidusSupport::EngineExtensions
   end
 end

bbaf85f7c287f38ee9f128ce9c877f766e34bd7e
benjamin wil <benjamin@super.gd>
2024-01-15 16:20:39 -0800

    Refactor skipped refund feature spec
    While trying to make this spec pass and investigate an issue with
    tax calculations we found during manual testing, I wanted to refactor
    this spec so that:
    
    1. It would be more clear which line items we were reimbursing for.
    2. It would cover the case where a line item would be removed as well as
       the case where a line item would not be removed but its quantity
       would be changed
    
    This test is pending as the reimbursement amounts populated are
    currently wrong ($10.00 instead of $10.89):
    
        Failures:
    
          1) Refunding an order adds tax calculated by TaxJar to the order total
             Failure/Error: expect(find(".reimbursement-refund-amount")).to have_content("$10.89")
               expected to find text "$10.89" in "$10.00"
             # ./spec/features/spree/admin/refund_spec.rb:72:in `block (2 levels) in <top (required)>'
    
    I removed the VCR cassette because we will need to re-record it as part
    of making the test pass.

--- spec/features/spree/admin/refund_spec.rb
+++ spec/features/spree/admin/refund_spec.rb
@@ -16,59 +16,130 @@ RSpec.feature "Refunding an order", :js do
     create :reimbursement_type,
       name: "original payment",
       type: "Spree::ReimbursementType::OriginalPayment"
+
+    # This will initially report the shipped order to TaxJar
+    perform_enqueued_jobs { order.recalculate }
   end
 
-  let(:order) { create(:shipped_order, line_items_attributes: [{}, {}, {quantity: 2}], number: "R525233498") }
+  # Each inventory unit in this order has a price of $10 before tax. Before the
+  # customer return, there are four units total, split across three line items.
+  #
+  let(:order) {
+    create :shipped_order,
+      line_items_attributes: [
+        {variant: create(:product, name: "First Product").master},
+        {variant: create(:product, name: "Second Product").master},
+        {quantity: 2, variant: create(:product, name: "Third Product").master}
+      ],
+      number: "R525233498",
+      shipment_cost: 0,
+      shipping_method: create(:free_shipping_method)
+  }
 
-  xit "adds tax calculated by TaxJar to the order total", js: true, vcr: {cassette_name: "features/spree/admin/refund", allow_unused_http_interactions: false} do
+  scenario "adds tax calculated by TaxJar to the order total",
+    js: true,
+    pending: "We are investigating why this spec is failing.",
+    vcr: {
+      cassette_name: "features/spree/admin/refund",
+      allow_unused_http_interactions: false
+    } do
     visit spree.admin_order_return_authorizations_path(order)
-
     click_on "New RMA"
-    check "return_authorization[return_items_attributes][0][_destroy]"
-    select "Montez's Warehouse", from: "Stock Location"
-    select "Original payment", from: "return_authorization[return_items_attributes][0][preferred_reimbursement_type_id]"
-    select "Defective", from: "return_authorization[return_items_attributes][0][return_reason_id]"
+
+    add_product_to_rma product_name: "First Product",
+      reimbursement_type: "Original payment",
+      reason: "Defective"
     click_on "Create"
 
+    # This customer return will remove an entire line item from the order.
+    #
     click_on "Customer Return"
     click_on "New Customer Return"
-    check "customer_return[return_items_attributes][0][returned]"
-    select "Received", from: "customer_return[return_items_attributes][0][reception_status_event]"
-    select "Montez's Warehouse", from: "Stock Location"
+    receive_return_item
     click_on "Create"
 
     click_on "Create reimbursement"
-    select "Original payment", from: "reimbursement[return_items_attributes][0][override_reimbursement_type_id]"
+    select "Original payment",
+      from: "reimbursement[return_items_attributes][0][override_reimbursement_type_id]"
     click_on "Update"
 
     perform_enqueued_jobs do
       click_on "Reimburse"
     end
 
+    # The reimbursement should be initialized with the correct amount to balance
+    # the order and mark it as "paid" (not "credit owed" or "balance due").
+    #
+    # It will be -1 * a single inventory unit: $10 + the TaxJar-calculated tax
+    # amount.
+    #
     expect(find(".reimbursement-refund-amount")).to have_content("$10.89")
 
     click_on "RMA"
-
     click_on "New RMA"
-    check "return_authorization[return_items_attributes][1][_destroy]"
-    select "Montez's Warehouse", from: "Stock Location"
-    select "Original payment", from: "return_authorization[return_items_attributes][1][preferred_reimbursement_type_id]"
-    select "Defective", from: "return_authorization[return_items_attributes][1][return_reason_id]"
+    add_product_to_rma product_name: "Third Product",
+      reimbursement_type: "Original payment",
+      reason: "Defective"
     click_on "Create"
 
+    # This customer return will *not* remove an entire line item from the
+    # order. The line item quantity was `2` and after this return it should be
+    # `1`.
+    #
     click_on "Customer Return"
     click_on "New Customer Return"
-    check "customer_return[return_items_attributes][0][returned]"
-    select "Received", from: "customer_return[return_items_attributes][0][reception_status_event]"
-    select "Montez's Warehouse", from: "Stock Location"
+    receive_return_item
     click_on "Create"
 
     click_on "Create reimbursement"
-    select "Original payment", from: "reimbursement[return_items_attributes][0][override_reimbursement_type_id]"
+    select "Original payment",
+      from: "reimbursement[return_items_attributes][0][override_reimbursement_type_id]"
     click_on "Update"
 
     perform_enqueued_jobs do
       click_on "Reimburse"
     end
+
+    # The reimbursement should be initialized with the correct amount to balance
+    # the order and mark it as "paid" (not "credit owed" or "balance due").
+    #
+    # It will be -1 * a single inventory unit: $10 + the TaxJar-calculated tax
+    # amount.
+    #
+    expect(find(".reimbursement-refund-amount")).to have_content("$10.89")
+  end
+
+  def add_product_to_rma(product_name:, reimbursement_type:, reason:, quantity: 1)
+    return_items_table = find "table.return-items-table"
+
+    within return_items_table do
+      rows_for_product =
+        find_all("tr").select { |row| row.text.include? product_name }
+
+      if rows_for_product.size < quantity
+        raise Capybara::ElementNotFound, "didn't find enough RMA table rows"
+      end
+
+      rows_for_product.first(quantity).each do |row|
+        within row do
+          checkbox = find "input[type='checkbox']"
+          checkbox.check
+
+          reimbursement_type_select = find "select[id$='reimbursement_type_id']"
+          reimbursement_type_select.find(:option, reimbursement_type).select_option
+
+          return_reason_select = find "select[id$='return_reason_id']"
+          return_reason_select.find(:option, reason).select_option
+        end
+      end
+    end
+
+    select "Montez's Warehouse", from: "Stock Location"
+  end
+
+  def receive_return_item
+    check "customer_return[return_items_attributes][0][returned]"
+    select "Received", from: "customer_return[return_items_attributes][0][reception_status_event]"
+    select "Montez's Warehouse", from: "Stock Location"
   end
 end
